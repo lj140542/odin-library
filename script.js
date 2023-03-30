6ee6c03e8bf82d212b83ea373a743ea091ef13fc
lj140542 <jonathan.lefebvre76600@gmail.com>
2023-03-31 00:33:22 +0900

    Implements the display of the library Array
    - creation of the display function for the Book class
    - creation of the display function for the library Array
    - attribution of some default values in the library Array
    - addition of a call to the library Array display function

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ function addBookToLibrary(title, author, pages, read) {
   library.push(new Book(title, author, pages, read));
 }
 
-
 Book.prototype.info = function () {
   let ret = '';
 
@@ -24,12 +23,56 @@ Book.prototype.info = function () {
 
   return ret;
 };
+Book.prototype.display = function () {
+  let book, main, title, author, other, pages, read;
+  let lib = document.getElementById('library');
+
+  // creation of the book
+  book = document.createElement('div');
+  book.classList.add('book');
+
+  // creation of the main-info 
+  title = document.createElement('span');
+  title.classList.add('title');
+  title.textContent = this.title;
+  author = document.createElement('span');
+  author.classList.add('author');
+  author.textContent = this.author;
+  main = document.createElement('div');
+  main.classList.add('main-info');
+  main.appendChild(title);
+  main.appendChild(author);
+
+  // creation of the other-info
+  pages = document.createElement('span');
+  pages.classList.add('pages');
+  pages.textContent = this.pages;
+  read = document.createElement('span');
+  read.classList.add('read');
+  read.textContent = this.read;
+  other = document.createElement('div');
+  other.classList.add('other-info');
+  other.appendChild(pages);
+  other.appendChild(read);
+
+  book.appendChild(main);
+  book.appendChild(other);
+  lib.appendChild(book);
+};
+
 library.toString = function () {
   library.forEach(function (book) {
     console.log(book.info());
   });
 };
+library.display = function () {
+  library.forEach(function (book) {
+    book.display();
+  });
+};
 
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '295', false);
-library.toString();
-
+addBookToLibrary('The Fellowship of the Ring', 'J.R.R. Tolkien', '295', true);
+addBookToLibrary('The Two Towers', 'J.R.R. Tolkien', '295', false);
+addBookToLibrary('The Return of the King', 'J.R.R. Tolkien', '295', false);
+library.display();
