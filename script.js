e6498e3aa7b8bc506a58f947448782eb2f61ad24
lj140542 <jonathan.lefebvre76600@gmail.com>
2023-03-31 21:49:21 +0900

    Modifies the display of the library Array
    - use append method instead of appendChild
    .. to add all child in one line
    - use prepend method to add the book div in the library div
    .. to take in consideration the 'add book' button that will be added
    - sort the library Array before display
    .. to display the books in alphabetical order

--- script.js
+++ script.js
@@ -40,8 +40,7 @@ Book.prototype.display = function () {
   author.textContent = this.author;
   main = document.createElement('div');
   main.classList.add('main-info');
-  main.appendChild(title);
-  main.appendChild(author);
+  main.append(title, author);
 
   // creation of the other-info
   pages = document.createElement('span');
@@ -52,12 +51,10 @@ Book.prototype.display = function () {
   read.textContent = this.read;
   other = document.createElement('div');
   other.classList.add('other-info');
-  other.appendChild(pages);
-  other.appendChild(read);
+  other.append(pages, read);
 
-  book.appendChild(main);
-  book.appendChild(other);
-  lib.appendChild(book);
+  book.append(main, other);
+  lib.prepend(book);
 };
 
 library.toString = function () {
@@ -66,6 +63,18 @@ library.toString = function () {
   });
 };
 library.display = function () {
+  // sort books by title (reversed for display) 
+  library.sort((a, b) => {
+    const titleA = a.title.toUpperCase();
+    const titleB = b.title.toUpperCase();
+    if (titleA < titleB) {
+      return 1;
+    }
+    if (titleA > titleB) {
+      return -1;
+    }
+    return 0;
+  });
   library.forEach(function (book) {
     book.display();
   });
@@ -75,4 +84,6 @@ addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '295', false);
 addBookToLibrary('The Fellowship of the Ring', 'J.R.R. Tolkien', '295', true);
 addBookToLibrary('The Two Towers', 'J.R.R. Tolkien', '295', false);
 addBookToLibrary('The Return of the King', 'J.R.R. Tolkien', '295', false);
+addBookToLibrary('Alice in Wonderland', 'Lewis Carroll', '295', true);
+addBookToLibrary('1984', 'George Orwell', '295', true);
 library.display();
